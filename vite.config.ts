0066a25562e1045c43134d2bf23f1cabcfff514a
Melishy <nazariktymkiv67@gmail.com>
2025-07-15 19:36:11 +0300

    fix issue with displaying NOTHINGNESS for github lmao i guess

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/visa-game/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
