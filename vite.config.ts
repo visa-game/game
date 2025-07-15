bc0a0d5bcccd31e96fc85df2aa71d87ee7776413
Melishy <nazariktymkiv67@gmail.com>
2025-07-15 20:08:08 +0300

    shit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/visa-game/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
